a35aed43701eabf516003249815d4d3aa9f28e75
Shuhaib77 <shuhaibbasheer20022@gmail.com>
2025-02-25 23:44:49 +0530

    updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,25 @@
-import React from 'react'
+import React from "react";
 
-function Header({setOpen,open}) {
+function Header({ setOpen, open }) {
   return (
     <div>
-        <div className='flex justify-between h-[50px] bg-[#7942af70] items-center '>
-           <h1 onClick={()=>{
-          open === false?setOpen(true):setOpen(false)
-            
-           }}>MENU</h1>
-             <h1>hii</h1>
-            <h1>hii</h1>
-            <h1>hii</h1>
-            <h1>hii</h1>
-            <h1>hii</h1>
-        </div>
+      <div className="flex justify-between h-[54px] bg-[#F2A154] items-center ">
+        <h1
+          className="cursor-pointer"
+          onClick={() => {
+            open === false ? setOpen(true) : setOpen(false);
+          }}
+        >
+          MENU
+        </h1>
+        <h1>hii</h1>
+        <h1>hii</h1>
+        <h1>hii</h1>
+        <h1>hii</h1>
+        <h1>hii</h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 function Sidebar() {
   return (
     <div>
-        <div className='w-[20vw] h-[100vh] '>
-           <div className=" h-[20vh] w-ful flex justify-around  bg-amber-500"> 
+        <div className='w-[19vw] h-[100vh] '>
+           <div className=" h-[6vh] w-ful flex justify-around items-center  bg-[#314E52]"> 
             <h1>ghjklmb</h1>
            </div>
-           <div className="h-[80vh] bg-[#7942af70] w-full flex flex-col justify-between items-center">
+           <div className="h-[95vh] bg-[#35A29F] w-full flex flex-col justify-between items-center">
             <h1>bhjm</h1>
             <h1>bhjm</h1>
             <h1>bhjm</h1>
