0ce49b56c1ddefe22075ce2538cfe628c158a117
Shuhaib77 <shuhaibbasheer20022@gmail.com>
2025-02-25 22:43:43 +0530

    updated

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <div className='flex justify-around h-[50px] bg-pink-600 items-center '>
+            <h1>hii</h1>
+            <h1>hii</h1>
+            <h1>hii</h1>
+            <h1>hii</h1>
+            <h1>hii</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header'
+
+function Home() {
+  return (
+    <div>
+        <div>
+            <Header/>
+        </div>
+    </div>
+  )
+}
+
+export default Home
